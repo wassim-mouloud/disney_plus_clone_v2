742c0e62665aaec24cf11e78afddc326039236a7
wassim-mouloud <simo.rko10@gmail.com>
2023-07-18 15:28:54 +0100

    adding useLocation to animate navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import {Link, useResolvedPath, useMatch} from 'react-router-dom'
+import {Link, useResolvedPath, useMatch, useLocation} from 'react-router-dom'
 import '../App.css';
 
 function Navbar() {
     const [active, setActive] = useState("Home")
+    const {pathname}= useLocation()
+    console.log(pathname)
 
     return (
         <div className=' group w-screen lg:hover:w-[20vw] lg:w-[80px] h-[85px] lg:h-screen fixed bottom-0 lg:top-0 lg:left-0 z-[99] bg-[#0f1013] lg:bg-transparent transition-[width] duration-500' style={{ backgroundImage: `linear-gradient(to right, #0f1013 , transparent)` }}>
@@ -27,27 +29,30 @@ function Navbar() {
                     </span>
                 </div>
 
-                {['Home', 'Search', "Movies", "Series"].map((item, i) => (
-                    <Link key={i} to={`${item!=='Home'?`/${item}`:'/'}`} className='ml-[50px] lg:ml-0 last:ml-0'> 
-                        
-                        <div
-                            key={item}
-                            className={`${active === item ? 'text-white' : 'text-[#C3C3C3]'} hover:text-white lg:hover:scale-[1.15] transition-transform duration-300 ${item==="Movies" || item==="Series"?"hidden  lg:flex" :'' } lg:ml-[10px] cursor-pointer h-[50px] flex flex-col gap-3 lg:flex-row items-center flex-shrink-0`}
-                            onClick={() => setActive(item)}
-                        >
-                            <img
-                                src={`/images/${active === item ? "white_" + item.toLowerCase() + ".png" : "gray_" + item.toLowerCase() + ".png"}`}
-                                alt=""
-                                className={`h-[20px] min-w-[20px] w-[20px] flex-shrink-0 object-cover`}
-                            />
-                            <span
-                                className={`lg:-translate-x-[70%] lg:opacity-0 flex-shrink-0 lg:group-hover:translate-x-3 lg:group-hover:opacity-100 transition-transform duration-500 text-[12px] lg:text-[18px]`}
+                {['Home', 'Search', "Movies", "Series"].map((item, i) => {
+                    return(
+                        <Link                                
+                        key={i} to={`${item!=='Home'?`/${item}`:'/'}`} className='ml-[50px] lg:ml-0 last:ml-0'> 
+                            
+                            <div
+                                className={`${(item!=='Home' && pathname === `/${item}`) || (item==='Home' && pathname==='/') ? 'text-white' : 'text-[#C3C3C3]'} hover:text-white lg:hover:scale-[1.15] transition-transform duration-300 ${item==="Movies" || item==="Series"?"hidden  lg:flex" :'' } lg:ml-[10px] cursor-pointer h-[50px] flex flex-col gap-3 lg:flex-row items-center flex-shrink-0`}
                             >
-                                {item}
-                            </span>
-                        </div>
-                    </Link>
-                ))}
+                                <img
+                                    src={`/images/${(item!=='Home' && pathname === `/${item}`) || (item==='Home' && pathname==='/') ? "white_" + item.toLowerCase() + ".png" : "gray_" + item.toLowerCase() + ".png"}`}
+                                    alt=""
+                                    className={`h-[20px] min-w-[20px] w-[20px] flex-shrink-0 object-cover`}
+                                />
+                                <span
+                                    className={`lg:-translate-x-[70%] lg:opacity-0 flex-shrink-0 lg:group-hover:translate-x-3 lg:group-hover:opacity-100 transition-transform duration-500 text-[12px] lg:text-[18px]`}
+                                >
+                                    {item}
+                                </span>
+                            </div>
+                        </Link>
+                    )
+                }
+
+                )}
             </div>
         </div>
     )

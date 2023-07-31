023008cea476b5a1b42c67e80ff2445013d4abb6
wassim-mouloud <simo.rko10@gmail.com>
2023-07-31 23:06:48 +0100

    changing scaling on slider hover to fix streched look

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -138,7 +138,7 @@ useEffect(()=>{
             })}
             {trending.map((movie, index) => {
                 return (
-                        <Link to={`/MovieDetail/${movie.id}`} key={index} onMouseEnter={() => handleMouseEnter(movie.id)} onMouseLeave={handleMouseLeave} className={`group  ${isHovered && movie.id===hoveredMovieId ? 'lg:hover:scale-x-[2] lg:hover:scale-y-[1.4]': ''}    bg-[#16181f] text-white  cursor-pointer lg:hover:z-[99] transition-transform duration-500 h-[170px] lg:h-[250px] lg:min-h-[250px] w-[110px] lg:w-[calc(100%/7-8px)] 2xl:w-[calc(100%/8-8px)] flex-shrink-0 rounded-[5px] ${index%7 === 0 ? "origin-left" : ''} ${index%7  === 6 && index!==0 ? "origin-right" : ''}`} >
+                        <Link to={`/MovieDetail/${movie.id}`} key={index} onMouseEnter={() => handleMouseEnter(movie.id)} onMouseLeave={handleMouseLeave} className={`group  ${isHovered && movie.id===hoveredMovieId ? 'lg:hover:scale-x-[1.7] lg:hover:scale-y-[1.4]': ''}    bg-[#16181f] text-white  cursor-pointer lg:hover:z-[99] transition-transform duration-500 h-[170px] lg:h-[250px] lg:min-h-[250px] w-[110px] lg:w-[calc(100%/7-8px)] 2xl:w-[calc(100%/8-8px)] flex-shrink-0 rounded-[5px] ${index%7 === 0 ? "origin-left" : ''} ${index%7  === 6 && index!==0 ? "origin-right" : ''}`} >
                        
 
                                 <img loading='lazy' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" className={`skeleton rounded-[5px] h-full w-full ${isHovered && movie.id===hoveredMovieId?'hidden  ':''}    `}/>

61bb5957244e274d07af56e067cac3fa465f8d7a
wassim-mouloud <simo.rko10@gmail.com>
2023-08-18 16:42:56 +0200

    changing title and description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,6 +15,20 @@ function Hero({ frontMovies }) {
     const [dupLast, setDupLast]= useState([])
     const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+    useEffect(()=>{
+            if(window.innerWidth>=1280){
+                setSlider([
+                    0,1,2,3,4,5,6,7,8,9,
+                    10,11,12,13,14,15,16,17
+                ])
+            }else if (window.innerWidth>=1024){
+                setSlider([
+                    0,1,2,3,4,5,6,7,8,9,
+                    10,11,12,13,14,15
+                ])
+            }
+    },[])
+
     useEffect(() => {
         if (frontMovies.length > 0) {
             if(window.innerWidth>=1536){
@@ -25,7 +39,7 @@ function Hero({ frontMovies }) {
                 setDupLast([11,12,13,14,15,16,17])
             }else if (window.innerWidth>=1024){
                 setDupFirst([0,1,2,3])
-                setDupLast([14,15,16,17])
+                setDupLast([12,13,14,15])
             }
     
         }
@@ -56,7 +70,7 @@ function Hero({ frontMovies }) {
                     sliderRef.current.style.setProperty("--slider-index", sliderIndex-1);
                     await sleep(500);
                     sliderRef.current.style.transition='none';
-                    sliderRef.current.style.setProperty("--slider-index", 3);
+                    sliderRef.current.style.setProperty("--slider-index", window.length>=1280?3:4);
                 }else{
                     sliderRef.current.style.transition='transform 0.5s linear';
                     sliderRef.current.style.setProperty("--slider-index", sliderIndex-1);
@@ -72,14 +86,25 @@ function Hero({ frontMovies }) {
         try{
             if(sliderRef.current){
                 const sliderIndex=parseInt(getComputedStyle(sliderRef.current).getPropertyValue("--slider-index"))
-    
-                if(sliderIndex===3){
-                    sliderRef.current.style.transition='transform 0.5s linear'
-                    sliderRef.current.style.setProperty("--slider-index", sliderIndex+1);
-                    await sleep(500)
-                    sliderRef.current.style.transition='none'
-                    sliderRef.current.style.setProperty("--slider-index", 1);
-                }else{
+                if(window.length>=1280){
+                    if(sliderIndex===3){
+                        sliderRef.current.style.transition='transform 0.5s linear'
+                        sliderRef.current.style.setProperty("--slider-index", sliderIndex+1);
+                        await sleep(500)
+                        sliderRef.current.style.transition='none'
+                        sliderRef.current.style.setProperty("--slider-index", 1);
+                    }else{
+                        if(sliderIndex===4){
+                            sliderRef.current.style.transition='transform 0.5s linear'
+                            sliderRef.current.style.setProperty("--slider-index", sliderIndex+1);
+                            await sleep(500)
+                            sliderRef.current.style.transition='none'
+                            sliderRef.current.style.setProperty("--slider-index", 1);
+                        }
+                    }
+                    
+                }
+            else{
                     sliderRef.current.style.transition='transform 0.5s linear'
                     sliderRef.current.style.setProperty("--slider-index", sliderIndex+1);
                 }

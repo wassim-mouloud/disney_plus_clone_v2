36740db04f636f9902042e88200501e4a22edbe2
wassim-mouloud <simo.rko10@gmail.com>
2023-09-02 02:59:01 +0200

    fixing MainSlider speed

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -38,7 +38,7 @@ function MainSlider({ trending, title }) {
       if(index >= 4) return
       let size = sliderRef.current.offsetWidth;
       let newIndex = index + 1;
-      sliderRef.current.style.transition = 'transform 0.2s linear';
+      sliderRef.current.style.transition = 'transform 0.6s linear';
       setIndex(newIndex);
       sliderRef.current.style.transform = 'translateX(' + (-size * newIndex) + 'px)';
   }
@@ -47,7 +47,7 @@ function MainSlider({ trending, title }) {
       if(index <= 0) return
       let size = sliderRef.current.offsetWidth;
       let newIndex = index - 1;
-      sliderRef.current.style.transition = 'transform 0.2s linear';
+      sliderRef.current.style.transition = 'transform 0.6s linear';
       setIndex(newIndex);
       sliderRef.current.style.transform = 'translateX(' + (-size * newIndex) + 'px)';
   }
